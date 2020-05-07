f826f82ae2ca1ee644098ce4578ad22d8bb3357c
Rodrigo Tenorio de Boa Ventura <boaventura19@yahoo.com.br>
2020-05-07 17:24:39 -0300

    excluido classLocalDateTimeA... e incluido equal e hasc na class transaction.

--- src/main/java/br/com/acqio/models/Transaction.java
+++ src/main/java/br/com/acqio/models/Transaction.java
@@ -1,5 +1,6 @@
 package br.com.acqio.models;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -18,7 +19,7 @@ import br.com.acqio.util.CardApplication;
 import br.com.acqio.util.Status;
 
 @Entity
-public class Transaction {
+public class Transaction implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -75,6 +76,29 @@ public class Transaction {
 	}
 	public void setStatus(Status status) {
 		this.status = status;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Transaction other = (Transaction) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	
