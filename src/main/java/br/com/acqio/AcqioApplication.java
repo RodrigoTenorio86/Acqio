2ea5e9d4e9ea89b5b5452961333838eced8d6e33
Rodrigo Tenorio de Boa Ventura <boaventura19@yahoo.com.br>
2020-05-22 19:53:56 -0300

    config do swagger na aplicacao.

--- src/main/java/br/com/acqio/AcqioApplication.java
+++ src/main/java/br/com/acqio/AcqioApplication.java
@@ -6,9 +6,12 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EntityScan(basePackageClasses = { AcqioApplication.class, Jsr310JpaConverters.class })
 @EnableSpringDataWebSupport
+@EnableSwagger2
 public class AcqioApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/acqio/endpoint/TransactionController.java
+++ src/main/java/br/com/acqio/endpoint/TransactionController.java
@@ -14,6 +14,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +30,7 @@ import br.com.acqio.models.Transaction;
 import br.com.acqio.models.dto.TransactionInDTO;
 import br.com.acqio.service.TransactionService;
 
+//@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
 public class TransactionController {
@@ -66,7 +68,7 @@ public class TransactionController {
 		return new ResponseEntity<>(transaction, HttpStatus.OK);
 	}
 	
-	
+
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<?> deleteById(@PathVariable("id") Long id){
 		verifyIfTransactionExists(id);
